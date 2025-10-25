f7af3cb948e923ebd8487d6eb452ab9c9b3b74ac
purkkapunkki <daftm4il@hotmail.com>
2025-10-26 00:01:03 +0300

    Refactor the code
    Multiple fixes, including:
    - Added the missing footer contents
    - Removed comments
    - Simplified css rules

--- js/login.js
+++ js/login.js
@@ -1,15 +1,4 @@
- // Näytetään logout-viesti, kun käyttäjä on kirjautunut ulos
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get("loggedout") === "true") {
-      document.getElementById("logoutMessage").textContent = "Olet kirjautunut ulos onnistuneesti.";
-    }
-
-    // Login demo
-    document.getElementById("loginForm").addEventListener("submit", function (e) {
-      e.preventDefault();
-      alert("Kirjautuminen onnistui (demo).");
-      //alku testi että sisään kirjautuminen toimii
-      window.location.href = "menu.html"; // Ohjataan käyttäjä menu.html-sivulle 
-      // mutta toisaalta pitäisi olla sitten 
-      // se non hidden/ signed in sivu....
-    });
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  window.location.href = "main.html";
+});
---
+++ js/register.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    window.location.href = "login.html";
+  });
