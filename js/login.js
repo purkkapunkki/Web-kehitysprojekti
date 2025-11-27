dd0761f4085b09129644b61b8d91fe561782bede
purkkapunkki <daftm4il@hotmail.com>
2025-11-27 19:17:38 +0200

    Modified and changed the name of function updateLoginIcon to updateLoginStatus. It now changes icon and text.

--- js/login.js
+++ js/login.js
@@ -3,10 +3,12 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
   window.location.href = "main.html";
 });
 
-function updateLoginIcon(isLoggedIn) {
+function updateLoginStatus(isLoggedIn) {
   const loginIcon = document.getElementById("login-icon");
+  const loginText = document.getElementById("login-text");
 
   if (isLoggedIn) {
     loginIcon.className = "fa-solid fa-xl fa-arrow-right-from-bracket";
+    loginText.textContent = "Kirjaudu ulos";
   }
 }
