f8d3c1b69c78f864ec59e057a6c5ef689fdef88b
purkkapunkki <daftm4il@hotmail.com>
2025-11-27 19:05:29 +0200

    Added initial updateLoginIcon

--- js/login.js
+++ js/login.js
@@ -2,3 +2,11 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
   e.preventDefault();
   window.location.href = "main.html";
 });
+
+function updateLoginIcon(isLoggedIn) {
+  const loginIcon = document.getElementById("login-icon");
+
+  if (isLoggedIn) {
+    loginIcon.className = "fa-solid fa-xl fa-arrow-right-from-bracket";
+  }
+}
